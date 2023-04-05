6f4333fa576403c87c4714e6ef572b250d0c0f0e
Fernando Cordeiro <fernando_cordeiro@live.com>
2023-04-05 13:52:15 +0100

    📦 NEW: Signup API server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
+const DB = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@docs.rwkhrmh.mongodb.net/?retryWrites=true&w=majority`;
 
 const app = express();
 
+// Middlewares
+// Pparses incoming JSON requests and puts the parsed data in req.body
+app.use(express.json());
+
+// Routes
+app.use(authRouter);
+
+// Connections
+mongoose
+	.connect(DB)
+	.then(() => {
+		console.log("Connected to MongoDB");
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
---
+++ server/models/user.js
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  profilePic: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = model("User", userSchema);
+export default User;
